b7b41629b6893fbd807863d4dcf731bbf418c750
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-08-25 18:08:45 +0200

    completed 6.19

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notificationReducer)
+const Notification = (props) => {
+  const notification = props.notification
   const getStyle = () => {
     if (notification === ''){
       return {
@@ -23,5 +23,10 @@ const Notification = () => {
     </div>
   )
 }
-
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notificationReducer
+  }
+}
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
